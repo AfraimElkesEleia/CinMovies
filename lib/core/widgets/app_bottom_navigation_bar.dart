7950f93eece59d6eb88f329eff179176dd355bd9
Afraim Elkes Eleia <afraimeleia200@gmail.com>
2026-07-16 05:30:20 +0300

    update UI of bottom navigation bar

--- lib/core/widgets/app_bottom_navigation_bar.dart
+++ lib/core/widgets/app_bottom_navigation_bar.dart
@@ -51,28 +51,36 @@ class AppBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: const BoxDecoration(
-        color: AppColors.scaffoldBackground,
-        border: Border(top: BorderSide(color: AppColors.surfaceBorder)),
-      ),
-      child: SafeArea(
-        top: false,
-        child: SizedBox(
-          height: 68,
-          child: Row(
-            children: AppNavTab.values.map((tab) {
-              return Expanded(
-                child: _AppBottomNavigationItem(
-                  tab: tab,
-                  isSelected: tab == currentTab,
-                  onTap: () => onTabSelected(tab),
-                ),
-              );
-            }).toList(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final itemWidth = constraints.maxWidth / AppNavTab.values.length;
+        final aiButtonSize = (itemWidth * 0.52).clamp(40.0, 48.0);
+
+        return DecoratedBox(
+          decoration: const BoxDecoration(
+            color: AppColors.scaffoldBackground,
+            border: Border(top: BorderSide(color: AppColors.surfaceBorder)),
           ),
-        ),
-      ),
+          child: SafeArea(
+            top: false,
+            child: SizedBox(
+              height: 68,
+              child: Row(
+                children: AppNavTab.values.map((tab) {
+                  return Expanded(
+                    child: _AppBottomNavigationItem(
+                      tab: tab,
+                      isSelected: tab == currentTab,
+                      aiButtonSize: aiButtonSize,
+                      onTap: () => onTabSelected(tab),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -81,15 +89,18 @@ class _AppBottomNavigationItem extends StatelessWidget {
   const _AppBottomNavigationItem({
     required this.tab,
     required this.isSelected,
+    required this.aiButtonSize,
     required this.onTap,
   });
 
   final AppNavTab tab;
   final bool isSelected;
+  final double aiButtonSize;
   final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
+    final isAiTab = tab == AppNavTab.ai;
     final itemColor = isSelected ? AppColors.loginPrimary : AppColors.iconMuted;
 
     return InkWell(
@@ -97,26 +108,30 @@ class _AppBottomNavigationItem extends StatelessWidget {
       child: Stack(
         alignment: Alignment.topCenter,
         children: [
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 200),
-            width: isSelected ? 24 : 0,
-            height: 2,
-            decoration: BoxDecoration(
-              color: AppColors.loginPrimary,
-              borderRadius: BorderRadius.circular(999),
+          if (!isAiTab)
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              width: isSelected ? 24 : 0,
+              height: 2,
+              decoration: BoxDecoration(
+                color: AppColors.loginPrimary,
+                borderRadius: BorderRadius.circular(999),
+              ),
             ),
-          ),
           Padding(
-            padding: const EdgeInsets.only(top: 12),
+            padding: EdgeInsets.only(top: isAiTab ? 0 : 12),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Icon(
-                  isSelected ? tab.activeIcon : tab.icon,
-                  size: 24,
-                  color: itemColor,
-                ),
-                const SizedBox(height: 4),
+                if (isAiTab)
+                  _AiNavigationIcon(isSelected: isSelected, size: aiButtonSize)
+                else
+                  Icon(
+                    isSelected ? tab.activeIcon : tab.icon,
+                    size: 24,
+                    color: itemColor,
+                  ),
+                SizedBox(height: isAiTab ? 2 : 4),
                 Text(
                   tab.label,
                   maxLines: 1,
@@ -131,6 +146,52 @@ class _AppBottomNavigationItem extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _AiNavigationIcon extends StatelessWidget {
+  const _AiNavigationIcon({required this.isSelected, required this.size});
+
+  final bool isSelected;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, -size * 0.35),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: isSelected
+                ? const [AppColors.loginPrimary, AppColors.comingSoonPurple]
+                : const [AppColors.surfaceBorder, AppColors.surface],
+          ),
+          borderRadius: BorderRadius.circular(size * 0.34),
+          border: isSelected
+              ? null
+              : Border.all(color: AppColors.surfaceBorder),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: AppColors.loginPrimary.withValues(alpha: 0.38),
+                    blurRadius: size * 0.45,
+                    offset: const Offset(0, 8),
+                  ),
+                ]
+              : null,
+        ),
+        child: Icon(
+          isSelected ? Icons.auto_awesome_rounded : Icons.auto_awesome_outlined,
+          color: AppColors.white,
+          size: size * 0.45,
+        ),
       ),
     );
   }
